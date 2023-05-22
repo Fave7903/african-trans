2d8e3daa4336e5f1a277011ff53468a671cd4254
Favour Solomon <femisolomon15@gmail.com>
2023-05-23 00:08:17 +0100

    included navbar and links

---
+++ faith-africa/src/components/Events.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Events = () => {
+  return (
+    <div>
+        <h1 className='text-4xl py-12'>Events </h1>
+        
+    </div>
+  )
+}
+
+export default Events
