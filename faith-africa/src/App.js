e9bd1c880dcc5aafe540b8697f434883ad73968d
Favour Solomon <femisolomon15@gmail.com>
2023-12-29 17:39:18 +0100

    update for live

--- faith-africa/src/App.js
+++ faith-africa/src/App.js
@@ -4,6 +4,7 @@ import Nav from './components/Nav';
 import About from './components/About';
 import Contact from './components/Contact';
 import Events from './components/Events';
+import Footer from './components/Footer';
 
 
 
@@ -13,68 +14,71 @@ function App() {
 
 
 
-<div id='hero'>
+    <div id='hero'>
 
 
-<section class="py-7">
-<Nav />
+    <section class="py-7">
+    <Nav />
 
 
-    <div
-      class="relative overflow-hidden bg-no-repeat bg-cover"
-      style={{backgroundPosition: "50%",
-        backgroundImage: `url('https://mdbcdn.b-cdn.net/img/new/slides/146.webp')`,
-        height: "500px"}}
-        
-      
-    >
-      <div
-        class="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
-        style={{backgroundColor: `rgba(0, 0, 0, 0.75)`}}
-      >
-        <div class="flex justify-center items-center h-full">
-          <div class="text-center text-white px-6 md:px-12">
-          <h1 class="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12 uppercase leading-[1]">
-           <span>We are </span>
-          <TypeAnimation sequence={[
-            'Agile',
-            2000,
-            'Adept',
-            2000,
-            'Africa',
-            2000,
-          ]}
-          speed={10}
-          wrapper='span'
-          className='text-blue-600/75'
-          repeat={Infinity}/>
-            </h1>
+        <div
+          class="relative overflow-hidden bg-no-repeat bg-cover"
+          style={{backgroundPosition: "50%",
+            backgroundImage: `url("https://aspyee.org/sites/default/files/2021-10/IT%20ED2.jpg")`,
+            height: "500px"}}
+            
+          
+        >
+          <div
+            class="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
+            style={{backgroundColor: `rgba(0, 0, 0, 0.75)`}}
+          >
+            <div class="flex justify-center items-center h-full">
+              <div class="text-center text-white px-6 md:px-12">
+              <h1 class="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12 uppercase leading-[1]">
+              <span>We are </span>
+              <TypeAnimation sequence={[
+                'Agile',
+                2000,
+                'Adept',
+                2000,
+                'Africa',
+                2000,
+              ]}
+              speed={10}
+              wrapper='span'
+              className='text-[#d97f38]'
+              repeat={Infinity}/>
+                </h1>
 
-            <a
-              href="#about"
-              type="button"
-              class="inline-block px-7 py-3 border-2 border-white text-white font-medium text-sm leading-snug uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-            >
-              Get started
-            </a>
+                <a
+                  href="#about"
+                  type="button"
+                  class="inline-block px-7 py-3 border-2 border-white text-white font-medium text-sm leading-snug uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+                  data-mdb-ripple="true"
+                  data-mdb-ripple-color="light"
+                >
+                  Get started
+                </a>
+              </div>
+            </div>
           </div>
         </div>
+      </section>
+    <div id='about' className='py-7 '>
+      <About />
       </div>
+    <div id='events' className='py-7 '>
+      <Events />
+      </div>
+
+    <div id="contact" className='py-7'></div>
+              <Contact />
     </div>
-  </section>
-<div id='about' className='py-7 '>
-  <About />
-  </div>
-<div id='events' className='py-7 '>
-  <Events />
-  </div>
 
-<div id="contact" className='py-7'></div>
-          <Contact />
-</div>
-
+    <div>
+              <Footer />
+    </div>
     </div>
   );
 }
--- faith-africa/src/components/About.js
+++ faith-africa/src/components/About.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Aos from "aos"
+import "aos/dist/aos.css"
 
 const About = () => {
+  useEffect(() => {
+    Aos.init({ duration: 2000});
+  }, []);
   return (
     <div>
-        <h1 className='text-4xl font-semibold py-12'>About</h1>
+      <div data-aos="fade-right">
+        <h1 className='text-6xl font-semibold py-12 text-[#d97f38]'>Our Mission</h1>
+      </div>
+        
+      <div data-aos="fade-left" className='flex justify-center'>
+        <h3 className='font-thin text-justify text-2xl mx-12 px-12 my-5'>At African Transformation Network (ATN), our mission is crystal clear: to empower African youths to shape a transformative future for themselves and the world. Through innovative programs and impactful initiatives, we pave the way for young African minds to thrive and contribute meaningfully on a global scale.</h3>
+      </div>
+      <div data-aos="fade-up" className='px-12 mx-12 my-5 text-justify'>
+      <ul className='grid grid-cols-2 gap-10'>
+          <li><h5 className='text-2xl font-semibold text-[#d97f38] mx-5 px-5'>Organizing Programs that propel growth</h5><p className='font-thin text-1xl my-4'>We orchestrate an array of transformative events, from thought-provoking webinars and mentorship opportunities to immersive masterclasses and inspiring conferences. These avenues serve as dynamic platforms for learning, networking, and honing skills essential for personal and professional growth.</p></li>
+          <li><h5 className='text-2xl font-semibold text-[#d97f38] mx-5 px-5'>Creating Compelling Content for Change</h5><p className='font-thin text-1xl my-4'>Harnessing the power of media, we curate engaging videos, insightful books, and specialized trainings. These resources are designed to empower and equip African youths with the knowledge and tools needed to succeed in today's ever-evolving world. Some of these training opportunities are available at a nominal cost to support our initiatives.</p></li>
+        </ul>
+        <h4 className='text-2xl font-semibold text-[#d97f38] mx-5 px-5'>Join Us in Redefining Possibilities</h4><p className='font-thin text-1xl my-4'>Join us on this transformative journey as we nurture the potential of African youths and cultivate a generation ready to make a meaningful impact. Together, we redefine possibilities and shape a future where African talent plays a pivotal role in global transformation.</p>
+      </div>
+       
         
     </div>
   )
